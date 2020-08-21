ce7b62178d9b1fbd5067b33c6ad11a9e4ddf9eee
Ivan <alexandrovis@gmail.com>
2020-08-21 17:22:50 +0300

    Добавляем второй урок

--- src/ru/geekbrains/lesson1/Task1.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.lesson1;
-
-public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("`lesson1_Task1");
-    }
-}
--- src/ru/geekbrains/lesson1/Task2.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.lesson1;
-
-public class Task2 {
-    public static void main(String[] args) {
-        System.out.println("Lesson1_Task2");
-    }
-}
