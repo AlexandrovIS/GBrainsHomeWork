d33ed0a2161552baf71a21c01d626ae529f88d3a
Ivan <alexandrovis@gmail.com>
2020-08-24 04:53:33 +0300

    ДЗ ко второму уроку, 6-е задание. Переделал заново! До этого точно было не верно.

--- src/ru/geekbrains/lesson2/Task6.java
+++ src/ru/geekbrains/lesson2/Task6.java
@@ -1,24 +1,21 @@
 package ru.geekbrains.lesson2;
 
 public class Task6 {
-    int[] arr ={10,10,10,2,3};
+    int[] arr = {2,1,2,2,1,1,1,10,};
+
     public static void main(String[] args) {
-        Task6 array=new Task6();
+        Task6 array = new Task6();
         array.myArray();
     }
-    private void myArray(){
-      int counter=0;
-        for (int i = 0; i < arr.length - 1; i++) {
-        int summ=0;
-        counter = counter + arr[i];
-          for (int j = arr.length - 1; j > 0; j--) {
-          summ = summ + arr[j];
-             if(summ==counter){
-             System.out.println(true);
-             }
-          }
+
+    public void myArray() {
+        for (int n = 0; n < arr.length - 1; n++) {
+            int counter = 0;
+            int summ = 0;
+            for (int i = 0; i < n+1; i++) counter += arr[i];
+            for (int j = arr.length - 1; j > n; j--) summ +=arr[j];
+            if (counter == summ) System.out.print(true);
         }
-
     }
-
 }
+
